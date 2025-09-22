a578820a921d8f98c367456859455c1ae00c846b
l1nk26 <fabianmtovar26@gmail.com>
2025-09-22 15:51:25 -0400

    added test for directed graph

--- tests/changingVerticesTest.cpp
+++ tests/changingVerticesTest.cpp
@@ -45,6 +45,45 @@ int main() {
 
     assert(emptyGraph.order() == 0);
     assert(emptyGraph.size() == 0);
+    
+    emptyDirectedGraph.addVertex("A");
+    emptyDirectedGraph.addVertex("B");
+    emptyDirectedGraph.addVertex("C");
+    emptyDirectedGraph.addVertex("D");
+ 
+    assert(emptyDirectedGraph.order() == 4);
+    assert(emptyDirectedGraph.size() == 0);
+    assert(emptyDirectedGraph.existVertex("A"));
+    assert(emptyDirectedGraph.existVertex("B"));
+    assert(emptyDirectedGraph.existVertex("C"));
+    assert(emptyDirectedGraph.existVertex("D"));
 
+    emptyDirectedGraph.addEdge("A", "B");
+    emptyDirectedGraph.addEdge("A", "C");
+    emptyDirectedGraph.addEdge("C", "B");
+    emptyDirectedGraph.addEdge("D", "A");
+
+    assert(emptyDirectedGraph.order() == 4);
+    assert(emptyDirectedGraph.size() == 4);
+    assert(emptyDirectedGraph.existEdge("A", "B"));
+    assert(emptyDirectedGraph.existEdge("A", "C"));
+    assert(emptyDirectedGraph.existEdge("C", "B"));
+    assert(emptyDirectedGraph.existEdge("D", "A"));
+
+    emptyDirectedGraph.removeEdge("A", "B");
+    assert(!emptyDirectedGraph.existEdge("A", "B"));
+    assert(emptyDirectedGraph.size() == 3);
+
+    std::cout << emptyDirectedGraph << std::endl;
+    emptyDirectedGraph.removeVertex("A");
+    std::cout << emptyDirectedGraph << std::endl;
+    emptyDirectedGraph.removeVertex("B");
+    std::cout << emptyDirectedGraph << std::endl;
+    emptyDirectedGraph.removeVertex("C");
+    emptyDirectedGraph.removeVertex("D");
+    std::cout << emptyDirectedGraph << std::endl;
+
+    assert(emptyDirectedGraph.order() == 0);
+    assert(emptyDirectedGraph.size() == 0);
     return 0;
 }
